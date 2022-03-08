fab5596b420754ec534564ba570858d67319289e
Mihaela-Loredana Ignat <mihaela-loredana.ignat@student.tuiasi.ro>
2022-03-09 01:05:46 +0200

    Delete unnecessary code

--- src/project/ProjectPIP.java
+++ src/project/ProjectPIP.java
@@ -5,10 +5,6 @@ public class ProjectPIP {
 
 	public static void main(String[] args) {
 		System.out.println("For repository");
-		int a,b,c;
-		a=0;
-		b=2;
-		c=a+b;
 		int[] sir=new int[6]; //secventa de cifre care va fi formata din cifrele 1,2,3 generate random
     	int[] arr={1,2,3};
     	for(int j=0;j<5;j++){
