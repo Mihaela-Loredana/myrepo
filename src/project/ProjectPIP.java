499f1ba302b9c18dd10b473057876f3fda9bb56f
Mihaela-Loredana Ignat <mihaela-loredana.ignat@student.tuiasi.ro>
2022-03-09 00:41:47 +0200

    Second commit - Added var

--- src/project/ProjectPIP.java
+++ src/project/ProjectPIP.java
@@ -4,6 +4,12 @@ public class ProjectPIP {
 
 	public static void main(String[] args) {
 		System.out.println("For repository");
+		int a,b,c;
+		a=0;
+		b=2;
+		c=a+b;
+		System.out.println("Add");
+	
 	}
 
 }
