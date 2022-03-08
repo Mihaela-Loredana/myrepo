a66cd28e2c57da4ad669bf2cf8bd60d78045abdf
Mihaela-Loredana Ignat <mihaela-loredana.ignat@student.tuiasi.ro>
2022-03-08 23:55:42 +0200

    First commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+#PIP-Project
+
+##PIP project in Java
+
+Loredana I.
+
+This repository is hosted at:
+
+https://github.com/Mihaela-Loredana/myrepo
---
+++ src/project/ProjectPIP.java
@@ -0,0 +1,9 @@
+package project;
+
+public class ProjectPIP {
+
+	public static void main(String[] args) {
+		System.out.println("For repository");
+	}
+
+}
